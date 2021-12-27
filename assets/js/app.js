078c7cdd669e67877ce13a1f6f4618cbaea4d2a8
Rana Ibraheem <rana.ibraheem82@gmail.com>
2021-12-27 20:35:35 +0100

    components added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,290 +1,27 @@
 let app = new Vue({
 	el: '#app',
+	props: {},
 	data: {
-
 		brand: '&#x1D554;&#x1D559;&#x1D556;&#x1D563;&#x1D55C;&#x1D55C;&#x1D560;&#x1D557;&#x1D557;&#x1D55A;&#x1D556;',
 		appName: 'Coffee Machines',
-		imagePath: '/assets/images/webshop/',
-		totalPrice: {
-			type: Number,
-			default: 0,
-		},
-		totalQuantity: {
-			type: Number,
-			default: 0,
-		},
+		machines: [],
 		shoppingCart: [],
 		machine_filter: 'all',
+		totalPrice: 0,
+		totalQuantity: 0,
 
-		machines: [{
-				name: "Simple and tasty",
-				image: "simplecoffem.jpg",
-				alt: "Photo by mentatdgt from Pexels",
-				machineNum: "CM357e",
-				text: "A simple coffee machine that makes a tasty coffee, takes no space in the kitchen.",
-				price: 45,
-				stock: 10,
-				id: 1,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Profitional",
-				image: "profitionalcoffem.jpg",
-				alt: "Photo by Eka Sutrisno from Pexels",
-				machineNum: "CM359e",
-				text: "For coffee lovers, this is the one you need.",
-				price: 899,
-				stock: 4,
-				id: 2,
-				quantity: 0,
-				category: 'bigmachine',
-				show: true,
-			},
-			{
-				name: "Fresh",
-				image: "freshcoffeem.jpg",
-				alt: "Photo by Matt Jerome Connor from Pexels",
-				machineNum: "CM356f",
-				text: "Always fresh coffee. With or without milk, so tasty.",
-				price: 150,
-				stock: 8,
-				id: 3,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Filter",
-				image: "filtercoffeem.jpg",
-				alt: "Photo by Thirdman from Pexels",
-				machineNum: "CM352f",
-				text: "A wonderfull coffee machine. For people who love filtered coffee, this is the best.",
-				price: 200,
-				stock: 12,
-				id: 4,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Easy",
-				image: "easycoffeem.jpg",
-				alt: "Photo by Ketut Subiyanto from Pexels",
-				machineNum: "CM363f",
-				text: "Nice set. Making coffee with this set is an easy and wonderfull experience.",
-				price: 450,
-				stock: 25,
-				id: 5,
-				quantity: 0,
-				category: 'bigmachine',
-				show: true,
-			},
-			{
-				name: "Silver",
-				image: "silvercoffeem.jpg",
-				alt: "Photo by Karolina Grabowska from Pexels",
-				text: "A beautifull silver coffee machine, tasty espresso and cappuccino.",
-				machineNum: "CM365f",
-				price: 130,
-				stock: 30,
-				id: 6,
-				quantity: 0,
-				category: 'bigmachine',
-				show: true,
-			},
-			{
-				name: "Small & Red",
-				image: "smallcoffeem.jpg",
-				alt: "Photo by Hiago Italo from Pexels",
-				machineNum: "CM368e",
-				text: "Small but tasty. A beautifull red coffee machine that makes a tasty coffee.",
-				price: 99,
-				stock: 0,
-				id: 7,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Take away and enjoy",
-				image: "takeawaycoffeem.jpg",
-				alt: "Photo by Taryn Elliott from Pexels",
-				machineNum: "CM369e",
-				text: "So tasty, so easy to use. Take it with you wherever you go.",
-				price: 55,
-				stock: 60,
-				id: 8,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Handy and tasty",
-				image: "handycoffeem.jpg",
-				alt: "Photo by Maria Orlova from Pexels",
-				machineNum: "CM363e",
-				text: "Handy and easy to use coffee machine. Makes very tasty coffee.",
-				price: 80,
-				stock: 20,
-				id: 9,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Super Machine",
-				image: "soprofitionalcoffeem.jpg",
-				alt: "Photo by Ivan Samkov from Pexels",
-				machineNum: "CM370f",
-				text: "A profitional coffee machine, for coffee lovers.",
-				price: 999,
-				stock: 8,
-				id: 10,
-				quantity: 0,
-				category: 'bigmachine',
-				show: true,
-			},
-			{
-				name: "Everyday Coffee",
-				image: "everydaycoffeem.jpg",
-				alt: "Photo by Cleyder Duque from Pexels",
-				machineNum: "CM373e",
-				text: "Tasty coffee for your everyday life, for you, for your guests, for everybody.",
-				price: 80,
-				stock: 15,
-				id: 11,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Modern",
-				image: "moderncoffeem.jpg",
-				alt: "Photo by Jonathan Borba from Pexels",
-				machineNum: "CM377f",
-				text: "Modern coffee machine, so beautifull and tasty.",
-				price: 199,
-				stock: 12,
-				id: 12,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Super easy",
-				image: "supereasycoffeem.jpg",
-				alt: "Photo by Svetlana Ponomareva from Pexels",
-				machineNum: "CM371e",
-				text: "Super easy coffee machine, for who wants tasty coffee fast.",
-				price: 50,
-				stock: 22,
-				id: 13,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Arabic",
-				image: "arabiccoffeem.jpg",
-				alt: "Photo by Michaela S. from Pexels",
-				machineNum: "CM378e",
-				text: "Set for fresh bewed arabic coffee, so tasty, you'll never regret it.",
-				price: 80,
-				stock: 10,
-				id: 14,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Super profitional",
-				image: "superprofitionalcoffeem.jpg",
-				alt: "Photo by Pavel Danilyuk from Pexels",
-				machineNum: "CM380f",
-				text: "Super profitional coffee machine. Tasty coffee, it's worth it.",
-				price: 1982,
-				stock: 2,
-				id: 15,
-				quantity: 0,
-				category: 'bigmachine',
-				show: true,
-			},
-			{
-				name: "Work time coffee",
-				image: "worktimecoffeem.jpg",
-				alt: "Photo by Ekrulila from Pexels",
-				machineNum: "CM490e",
-				text: "Tasty coffee. You can make it fast and start your work actively",
-				price: 75,
-				stock: 43,
-				id: 16,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Time out",
-				image: "timeoutcoffeem.jpg",
-				alt: "Photo by Rachel Claire from Pexels",
-				machineNum: "CM495e",
-				text: "To relax and take a time out and drink some tasty coffee.",
-				price: 160,
-				stock: 16,
-				id: 17,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Nice coffee",
-				image: "nicecoffeem.jpg",
-				alt: "Photo by Lynnelle Richardson from Pexels",
-				machineNum: "CM494e",
-				text: "Nice coffee machine that makes a tasty coffee for you.",
-				price: 399,
-				stock: 20,
-				id: 18,
-				quantity: 0,
-				category: 'bigmachine',
-				show: true,
-			},
-			{
-				name: "Coffee lovers",
-				image: "coffeeloverscoffeem.jpg",
-				alt: "Photo by Clam Lo from Pexels",
-				machineNum: "CM483e",
-				text: "If you love coffe, you will love this coffee machine.",
-				price: 275,
-				stock: 18,
-				id: 19,
-				quantity: 0,
-				category: 'smallmachine',
-				show: true,
-			},
-			{
-				name: "Cappuccino lovers",
-				image: "cappuccinoloverscoffeem.jpg",
-				alt: "Photo by Pavel Danilyuk from Pexels",
-				machineNum: "CM400f",
-				text: "This coffee machine is for you, espically for you, cappuccino lovers.",
-				price: 2560,
-				stock: 9,
-				id: 20,
-				quantity: 0,
-				category: 'bigmachine',
-				show: true,
-			}
-		],
 	},
 
 	created() {
+		this.machines = machines
+		// this.shoppingCart = this.cart
 		this.totalPrice = localStorage.getItem('totalPrice') !== null ? parseInt(localStorage.getItem('totalPrice')) : 0;
 		this.totalQuantity = localStorage.getItem('totalQuantity') !== null ? parseInt(localStorage.getItem('totalQuantity')) : 0;
 
 		localStorage.getItem('totalQuantity');
 		localStorage.getItem('totalPrice');
+
+
 	},
 
 	computed: {
@@ -292,13 +29,15 @@ let app = new Vue({
 			return this.brand + " " + this.appName
 		},
 		cart() {
-			return this.machines.filter(machine => machine.quantity > 0)
+			return this.shoppingCart = this.machines.filter(machine => machine.quantity > 0)
 		},
 	},
 
+
 	methods: {
-		updateCart(machine, updateType) {
+		addToCart(machine, updateType) {
 			for (let i = 0; i < this.machines.length; i++) {
+
 				if (this.machines[i].id === machine.id) {
 					if (updateType === 'substract') {
 						if (this.machines[i].quantity !== 0) {
@@ -325,13 +64,11 @@ let app = new Vue({
 						localStorage.setItem('totalQuantity', this.totalQuantity)
 						localStorage.setItem('totalPrice', this.totalPrice)
 					}
-
-					break;
 				}
 			}
 		},
 
-		remove() {
+		removeAll() {
 			this.shoppingCart.length = this.cart.length = 0
 			this.totalPrice = 0
 			this.totalQuantity = 0
@@ -348,7 +85,7 @@ let app = new Vue({
 			}
 		},
 
-		removeMachine(index) {
+		removeItem(index) {
 			this.totalQuantity -= this.shoppingCart[index].quantity
 			this.totalPrice -= this.shoppingCart[index].price * this.shoppingCart[index].quantity
 			this.shoppingCart[index].stock += this.shoppingCart[index].quantity
@@ -369,16 +106,9 @@ let app = new Vue({
 					element.show = element.category != this.machine_filter ? false : true;
 				});
 			}
-		},
-	},
 
-	watch: {
-		shoppingCart: {
-			handler(newUpdate) {
-				localStorage.shoppingCart = JSON.stringify(newUpdate);
-			},
-			deep: true
 		},
+
 	},
 
 	mounted() {
@@ -393,26 +123,26 @@ let app = new Vue({
 				}
 			})
 		})
+		this.$on('update-cart', (machine, updateType) => {
+			this.addToCart(machine, updateType)
+		})
+		this.$on('remove', () => {
+			this.removeAll()
+		})
+		this.$on('remove-machine', (index) => {
+			this.removeItem(index)
+		})
+	},
+
+	watch: {
+		shoppingCart: {
+			handler(newUpdate) {
+				localStorage.shoppingCart = JSON.stringify(newUpdate);
+			},
+			deep: true
+		},
 	},
 })
 
 Vue.config.devtools = true
 Vue.config.productionTip = false
-
-
-
-
-
-
-
-// let app = new Vue({
-//     el: '#app',
-
-//     data: {
-//         appName: 'Coffee Machines',
-//         products: [],
-//     },
-//     })
-
-// Vue.config.devtools = true
-// Vue.config.productionTip = false
---
+++ assets/js/components/products.js
@@ -0,0 +1,52 @@
+Vue.component('products', {
+    data() {
+        return {
+            machineItems: [],
+            imagePath: '/assets/images/webshop/',
+
+        }
+    },
+    props: {},
+    created() {
+        this.machineItems = machines;
+
+    },
+    computed: {
+
+    },
+
+    template: `
+    <section class="products block">
+    <div class="card container-fluid col-md-5" v-for="machine in machineItems" :key="machine.id" v-show="machine.show">
+        <a :href="imagePath + machine.image"> <img :src="imagePath+machine.image" class="card-img-top"
+                :alt="machine.alt"></a>
+        <div class="card-body">
+            <h5>{{machine.name}}</h5>
+            <h5>{{machine.machineNum}}</h5>
+            <p class="card-text">{{machine.text}}</p>
+            <p>Price: {{machine.price}}$</p>
+            <p class="soldOut" v-if="machine.stock===0">Sold Out</p>
+            <p class="soldOut" v-else-if="machine.stock<=5 && machine.stock>0">Almost Sold Out</p>
+            <p v-else-if="machine.stock>5">In Stock</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart"
+                viewBox="0 0 16 16">
+                <path
+                    d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            </svg>
+            <button class="addToCart btn btn-primary" @click="updateCart(machine,'substract')"
+                :disabled="machine.stock === 0 && machine.quantity===0"
+                :class="{disabledButton: machine.stock===0 && machine.quantity===0}">-</button>
+            <span>{{machine.quantity}}</span>
+            <button class=" addToCart btn btn-primary" @click="updateCart(machine,'add')"
+                :disabled="machine.stock === 0" :class="{disabledButton: machine.stock === 0}">+</button>
+        </div>
+    </div>
+</section>
+
+    `,
+    methods: {
+        updateCart(machine, updateType) {
+            this.$root.$emit('update-cart', machine, updateType)
+        },
+    }
+})
