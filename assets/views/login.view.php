c3a8236f6f371fbb636129b84e7dc07e8ec50de6
ranaibraheem <rana.ibraheem82@gmail.com>
2022-01-20 15:18:40 +0100

    login

--- assets/views/login.view.php
+++ assets/views/login.view.php
@@ -1,13 +1,13 @@
 <div class="form-group">
-            <form method="POST" action="?page=register&action=store" id="logIn" class="row g-3">
+            <form method="POST" action="?page=login&action=login" id="logIn" class="row g-3">
                 <div class="col-md-6">
                     <label for="inputEmail4" class="label">Email</label>
-                    <input v-model="email" type="email" class="form-control" id="inputEmail4" required>
+                    <input type="email" name="email" class="form-control" id="inputEmail4" required>
                 </div>
 
                 <div class="col-md-6">
                     <label for="inputPassword4" class="label">Password</label>
-                    <input v-model="password" type="password" class="form-control" id="inputPassword4" required>
+                    <input type="password" name="password" class="form-control" id="inputPassword4" required>
                 </div>
                 <div class="col-12">
                     <button type="submit" class="btn btn-primary">Log in</button>
